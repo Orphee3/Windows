df110ee82947890cdab2bd35671ad26a5f98e349
Mickael45 <mickachoux12@gmail.com>
2015-09-06 17:47:07 +0200

    User's ProfilePage UI done

--- Orphee/Orphee/Orphee.Windows/Views/ProfilePage.xaml.cs
+++ Orphee/Orphee/Orphee.Windows/Views/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Practices.Prism.Mvvm;
+﻿using Windows.UI.Xaml.Input;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace Orphee.Views
 {
@@ -8,6 +9,11 @@ namespace Orphee.Views
         {
             this.InitializeComponent();
       
+        }
+
+        private void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
