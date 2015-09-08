ccc64f6f07935b23e703e996d38eda0a4596c1b3
Mickael45 <mickachoux12@gmail.com>
2015-09-08 14:53:13 +0200

    ProfilePage's user picture background changes to take the value of the dominant color of the picture

--- Orphee/Orphee/Orphee.Shared/ViewModels/Interfaces/IProfilePageViewModel.cs
+++ Orphee/Orphee/Orphee.Shared/ViewModels/Interfaces/IProfilePageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Microsoft.Practices.Prism.Commands;
 
 namespace Orphee.ViewModels.Interfaces
@@ -10,6 +12,8 @@ namespace Orphee.ViewModels.Interfaces
         int NumberOfComments { get; }
         int NumberOfFollows { get; }
         int NumberOfFollowers { get; }
+        string UserPictureSource { get; }
+        SolidColorBrush BackgroundPictureColor { get; }
         Visibility DisconnectedStackPanelVisibility { get; }
         Visibility ConnectedStackPanelVisibility { get; }
         DelegateCommand LoginCommand { get; }
--- Orphee/Orphee/Orphee.Shared/ViewModels/ProfilePageViewModel.cs
+++ Orphee/Orphee/Orphee.Shared/ViewModels/ProfilePageViewModel.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.UI;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
@@ -16,6 +22,18 @@ namespace Orphee.ViewModels
         public int NumberOfComments { get; private set; }
         public int NumberOfFollows { get; private set; }
         public int NumberOfFollowers { get; private set; }
+        public string UserPictureSource { get; private set; }
+        private SolidColorBrush _backgroundColorBrush;
+
+        public SolidColorBrush BackgroundPictureColor
+        {
+            get { return this._backgroundColorBrush; }
+            set
+            {
+                if (this._backgroundColorBrush != value)
+                    SetProperty(ref this._backgroundColorBrush, value);
+            }
+        }
         public Visibility DisconnectedStackPanelVisibility { get; private set; }
         public Visibility ConnectedStackPanelVisibility { get; private set; }
         public DelegateCommand LoginCommand { get; private set; }
@@ -23,6 +41,8 @@ namespace Orphee.ViewModels
         public ProfilePageViewModel()
         {
             SetPropertiesSependingOnConnectionState();
+            this.UserPictureSource = "/Assets/flower3.jpg";
+            InitBackgroundPictureColor();
             this.LoginCommand = new DelegateCommand(() => App.MyNavigationService.Navigate("Login", null));
         }
 
@@ -49,6 +69,53 @@ namespace Orphee.ViewModels
             {
                 this.DisconnectedStackPanelVisibility = Visibility.Visible;
                 this.ConnectedStackPanelVisibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void InitBackgroundPictureColor()
+        {
+            this.BackgroundPictureColor = new SolidColorBrush { Color = await SearchDominantPictureColor() };
+        }
+
+        private async Task<Color> SearchDominantPictureColor()
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx://" + this.UserPictureSource));
+
+            using (var stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                //Create a decoder for the image
+                var decoder = await BitmapDecoder.CreateAsync(stream);
+
+                //Create a transform to get a 1x1 image
+                var myTransform = new BitmapTransform { ScaledHeight = decoder.PixelHeight, ScaledWidth = decoder.PixelWidth };
+
+                //Get the pixel provider
+                var pixels = await decoder.GetPixelDataAsync(
+                    BitmapPixelFormat.Rgba8,
+                    BitmapAlphaMode.Straight,
+                    myTransform,
+                    ExifOrientationMode.RespectExifOrientation,
+                    ColorManagementMode.ColorManageToSRgb);
+
+                //Get the bytes of the 1x1 scaled image
+                var bytes = pixels.DetachPixelData();
+
+                int alpha = 0;
+                int red = 0;
+                int blue = 0;
+                int green = 0;
+                for (var i = 0; i < bytes.Length; i += 4)
+                {
+                    red += bytes[i];
+                    green += bytes[i + 1];
+                    blue += bytes[i + 2];
+                    alpha += bytes[i + 3];
+                }
+                var alphaByte = (byte)(alpha / (bytes.Length / 4));
+                var bytered = (byte)(red / (bytes.Length / 4));
+                var bytegreen = (byte)(green / (bytes.Length / 4));
+                var byteblue = (byte)(blue / (bytes.Length / 4));
+                return (Color.FromArgb(alphaByte, bytered, bytegreen, byteblue));
             }
         }
     }
