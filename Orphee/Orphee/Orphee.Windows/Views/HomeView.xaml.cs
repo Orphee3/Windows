c91927c087431bee326d0d47fde5d1dcf2daef91
Mickael45 <mickachoux12@gmail.com>
2015-09-08 18:11:16 +0200

    HomePage's Friend news only accessible if connected

--- Orphee/Orphee/Orphee.Windows/Views/HomeView.xaml.cs
+++ Orphee/Orphee/Orphee.Windows/Views/HomeView.xaml.cs
@@ -4,6 +4,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Microsoft.Practices.Prism.Mvvm;
 using Newtonsoft.Json.Linq;
+using Orphee.RestApiManagement;
 using Orphee.ViewModels;
 
 namespace Orphee.Views
@@ -25,9 +26,15 @@ namespace Orphee.Views
         {
             this._x2 = (int) e.Position.X;
             if (this._x1 > this._x2 && (this.PopularTextBox.Foreground as SolidColorBrush).Color == Color.FromArgb(100, 13, 71, 161))
-                ((HomePageViewModel)this.DataContext).FillFlowListWithNewFriendCreations();
-            else if (this._x1 < this._x2 && (this.FriendNewsTextBox.Foreground as SolidColorBrush).Color == Color.FromArgb(100, 13, 71, 161))
-                ((HomePageViewModel)this.DataContext).FillFlowListWithPopularCreations();
+            {
+                if (RestApiManagerBase.Instance.IsConnected)
+                    ((HomePageViewModel) this.DataContext).FillFlowListWithNewFriendCreations();
+                else
+                    App.MyNavigationService.Navigate("Login", null);
+            }
+            else if (this._x1 < this._x2 &&
+                     (this.FriendNewsTextBox.Foreground as SolidColorBrush).Color == Color.FromArgb(100, 13, 71, 161))
+                ((HomePageViewModel) this.DataContext).FillFlowListWithPopularCreations();
         }
 
         private void UIElement_OnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
