1babe093272fa23971758fd28a7cc5e659449020
Mickael45 <mickachoux12@gmail.com>
2015-09-08 17:41:19 +0200

    ChannelInfoPage's creation's commentary button bond to CreationInfoPage

--- Orphee/Orphee/Orphee.Windows/Views/ChannelInfoPage.xaml.cs
+++ Orphee/Orphee/Orphee.Windows/Views/ChannelInfoPage.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Input;
 using Microsoft.Practices.Prism.Mvvm;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Orphee.Views
 {
@@ -26,6 +12,11 @@ namespace Orphee.Views
         public ChannelInfoPage()
         {
             this.InitializeComponent();
+        }
+
+        private void CommentaryIcon_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            App.MyNavigationService.Navigate("CreationInfo", null);
         }
     }
 }
